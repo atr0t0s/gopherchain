0ea43fbe1a5177c7fa3111226c8be412a0e4d620
George Violaris <violarisgeorge@gmail.com>
2017-10-19 13:19:40 +0300

    Proof of work and fixes to genesis block

--- gopherchain.go
+++ gopherchain.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"os"
   "math/rand"
+	"encoding/binary"
 )
 
 type Block struct {
@@ -24,52 +25,48 @@ type Block struct {
 
 func genesis() Block {
 
-	var sha = sha256.New()
-	sha.Write([]byte("This is Gopher, building your chain!"))
-
-	genesisBlock := Block {0, time.Now(), "This is the Genesis block", "Genesis", hex.EncodeToString(sha.Sum(nil)), ""}
+	genesisPrevHash := "0000000000000000000000000000000000000000000000000000000000000000"
+	genesisHash := "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+	genesisBlock := Block {0, time.Now(), "This is the Genesis block", "Genesis", genesisPrevHash, genesisHash}
 
 	return genesisBlock
 
 }
 
 func nextBlock(lastBlock Block) Block {
-
 	blockIndex := lastBlock.index + 1
 	blockData := "This is block " + strconv.Itoa(blockIndex)
-	previousHash := lastBlock.previousHash
+	previousHash := lastBlock.thisHash
 	rand.Seed(time.Now().UnixNano())
 	nonce := make([]byte, 4)
 	rand.Read(nonce)
+	newnonce := binary.LittleEndian.Uint32(nonce)
+
 	var blockStringSum string
 	// <Proof of work>
 
 	for (previousHash != "") {
 		var sha = sha256.New()
+			str := fmt.Sprint(newnonce)
+			blockStringSum = str + previousHash
+			sha.Write([]byte(blockStringSum))
+			blockStringSum = hex.EncodeToString(sha.Sum(nil))
 
-		blockStringSum = previousHash + hex.EncodeToString(nonce)
-		sha.Write([]byte(blockStringSum))
+			fmt.Printf(blockStringSum)
+			fmt.Printf("\n")
 
-		fmt.Printf(blockStringSum)
-		fmt.Printf("\n")
-
-		blockStringSum = hex.EncodeToString(sha.Sum(nil))
-
-		fmt.Printf(blockStringSum)
-		fmt.Printf("\n\n")
-
-		if (blockStringSum[:3] == "000") {
-			break;
-		} else {
-			nonce[2]++
-			sha.Reset()
+			if (blockStringSum[:4] == "0000") {
+				break
+			} else {
+				newnonce++
+				sha.Reset()
+			}
 		}
-	}
 
 	t, err := os.Create("height")
 	check(err)
-	n, err := t.WriteString(strconv.Itoa(blockIndex))
-	fmt.Printf("Written height to file", n)
+	t.WriteString(strconv.Itoa(blockIndex))
+	fmt.Printf("\nWritten to file:\n")
 	t.Sync()
 
 	proof := hex.EncodeToString(nonce)
@@ -118,12 +115,12 @@ os.O_CREATE, 0600)
 		blockInfo := heightLabel + height + " \n" + blockLabel + block + "\n"
 		n, err := f.WriteString(blockInfo)
 		check(err)
-		fmt.Printf("wrote %d bytes\n", n)
+		fmt.Printf("wrote %d bytes\n\n", n)
 		f.Sync()
 
 		previousBlock = newBlock
 
-		time.Sleep(10000 * time.Millisecond) //simulate block creation by delaying output
+		time.Sleep(3000 * time.Millisecond) //simulate block creation by delaying output
 
 	}
 
